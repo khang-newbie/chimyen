c3eff44ae41154423e700e37b16273e1235ee9cc
khang-newbie <tranquockhang24242424@gmail.com>
2026-06-12 15:43:21 +0700

    Fix: Update Service Worker to cache essential files and handle fetch events

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/CTU_logo.png"]);
+      return cache.addAll(["/", "/index.html", "/style.css", "/script.js", "/manifest.json", "/CTU_logo.png"]);
     }),
   );
 });
-   
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
